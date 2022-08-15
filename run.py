3bee79c588ce7d4bdb2692243cd3e5592b5c61cf
TZbooo <helloworldbooo@gmail.com>
2022-08-15 22:16:23 +0300

    added log messages

--- run.py
+++ run.py
@@ -7,21 +7,30 @@ from parser.bloomberg.bloomberg_parser import BloombergParser
 from parser.config import ZEROHEDGE_ARTICLES_PAGE_URL, BLOOMBERG_ARTICLES_PAGE_URL
 from sender.telegram.bot import TelegramSender
 from sender.telegram.config import TELEGRAM_CHANNEL_ID, TELEGRAM_BOT_TOKEN
+from logger import logger
 
 
 def send_articles():
-    zerohedge_parser = ZeroHedgeParser(ZEROHEDGE_ARTICLES_PAGE_URL)
-    bloomberg_parser = BloombergParser(BLOOMBERG_ARTICLES_PAGE_URL)
+    logger.info('start init parsers')
+    zerohedge_parser = ZeroHedgeParser(ZEROHEDGE_ARTICLES_PAGE_URL, False)
+    logger.info('zerohedge parser init')
+    bloomberg_parser = BloombergParser(BLOOMBERG_ARTICLES_PAGE_URL, False)
+    logger.info('end init parsers')
+    logger.info('start parsing')
     zerohedge_article = zerohedge_parser.get_latest_article()
     bloomberg_article = bloomberg_parser.get_latest_article()
+    logger.info('end parsing')
+    logger.info('start sending')
     telegram_sender = TelegramSender(TELEGRAM_BOT_TOKEN, TELEGRAM_CHANNEL_ID)
     telegram_sender.send_article(zerohedge_article)
     telegram_sender.send_article(bloomberg_article)
+    logger.info('end sending')
 
 
 if __name__ == '__main__':
-    schedule.every().day.at('10:48').do(send_articles)
+    send_articles()
+    # schedule.every().day.at('20:31').do(send_articles)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    # while True:
+        # schedule.run_pending()
+        # time.sleep(1)
