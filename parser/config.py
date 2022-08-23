4e7f14d74b147eca2b29cd7f74a05f56c6479940
TZbooo <helloworldbooo@gmail.com>
2022-08-23 15:52:11 +0300

    i am sorry by idea i must create this commit in feature branch, but i do this, in .env file i find useless data, because it not secret and i move them to config.py

--- parser/config.py
+++ parser/config.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BLOOMBERG_ARTICLES_PAGE_URL = os.getenv('BLOOMBERG_ARTICLES_PAGE_URL')
-ZEROHEDGE_ARTICLES_PAGE_URL = os.getenv('ZEROHEDGE_ARTICLES_PAGE_URL')
+BLOOMBERG_ARTICLES_PAGE_URL='https://www.bloomberg.com/search?query=your%20evening%20briefing'
+ZEROHEDGE_ARTICLES_PAGE_URL='https://www.zerohedge.com'
